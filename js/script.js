5da92e768ab329c4758c301ef2a80db121342323
Natalia Veríssimo <82881439+Dev-Verissimo@users.noreply.github.com>
2022-06-14 19:40:42 -0300

    status-code_JS-parte2

--- js/script.js
+++ js/script.js
@@ -13,6 +13,27 @@ function inserirReceita() {
   );
 
   promessa.then(receitaCadastradaComSucesso);
+
+  promessa.catch(tratarErro);
+}
+
+function tratarErro(e) {
+  console.log(e, "erroooo");
+  if (e.response.status === 409) {
+    e.response.message = "Esse Título já existe";
+    alteraImagem(409);
+    console.log(e.response.message);
+  } else if (e.response.status === 422) {
+    e.response.message =
+      "ingredientes ou modo de preparo com menos de 10 caracteres";
+    alteraImagem(422);
+  }
+}
+
+function alteraImagem(status) {
+  let imagem = document.querySelector(".imagem-erro img");
+  imagem.setAttribute("src", "img/" + status + ".jpg");
+  console.log(imagem);
 }
 
 function receitaCadastradaComSucesso() {
